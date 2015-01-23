027694f17d0a507680270674b101beaccc7720bf
Dave Canam <david.canam@gmail.com>
2015-01-22 23:12:16 -0400

    added columns

--- src/js/play.js
+++ src/js/play.js
@@ -6,16 +6,18 @@
  */
 
 
-// var musicOn = true;
+var wKey,
+    aKey,
+    sKey,
+    dKey,
+    spaceKey;
 
-
-var wKey;
-var aKey;
-var sKey;
-var dKey;
-var spaceKey;
-
-var group, player, ground, background, scrollPosition = 0;
+var group,
+    player,
+    ground,
+    background,
+    scrollPosition = 0,
+    pillars;
 
 Game.Play = function(game) {
   this.game = game;
@@ -28,11 +30,11 @@ Game.Play.prototype = {
 
 
     //Draw a white square
-    var playerbmd = this.game.add.bitmapData(32, 32);
-        playerbmd.ctx.strokeStyle = '#000';
-        playerbmd.ctx.rect(0, 0, 32, 32);
-        playerbmd.ctx.fillStyle = '#fff';
-        playerbmd.ctx.fill();
+    this.playerbmd = this.game.add.bitmapData(32, 32);
+    this.playerbmd.ctx.strokeStyle = '#000';
+    this.playerbmd.ctx.rect(0, 0, 32, 32);
+    this.playerbmd.ctx.fillStyle = '#fff';
+    this.playerbmd.ctx.fill();
 
     //Draw a black and white checker board
     var groundbmd = this.game.add.bitmapData(32, 32);
@@ -46,6 +48,12 @@ Game.Play.prototype = {
         groundbmd.ctx.fillStyle = '#ff0000';
         groundbmd.ctx.fill();
 
+    var pillar = this.game.add.bitmapData(32, 64);
+        pillar.ctx.rect(0, 0, 32, 64);
+        pillar.ctx.fillStyle = '#fff';
+        pillar.ctx.fill();
+
+    this.pillars = this.game.add.group();
 
     background = this.game.add.tileSprite(0, 0, this.game.width, this.game.height, groundbmd);
     background.tileScale.set(4);
@@ -59,13 +67,14 @@ Game.Play.prototype = {
     ground.body.immovable = true;
     ground.body.allowGravity = false;
 
-    player = this.game.add.sprite(128, this.game.world.centerY, playerbmd, 0, group);
+    player = this.game.add.sprite(128, this.game.world.centerY, this.playerbmd, 0, group);
     this.game.physics.arcade.enable(player);
     player.anchor.set(0.5);
     // player.tint = 0x00ff00;
     player.tint = 0xffffff;
     player.body.gravity.y = 750;
 
+    this.timer = this.game.time.events.loop(1500, this.addPillars, this);  
 
     // // Music
     // this.music = this.game.add.sound('music');
@@ -83,15 +92,39 @@ Game.Play.prototype = {
     this.cursors = this.game.input.keyboard.createCursorKeys();
 
   },
+  addPillars: function() {
+    var hole = Math.floor(Math.random() * 5) + 1;
+     // Add the 6 pipes 
+     for (var i = 0; i < 9; i++)
+       if (i != hole && i != hole + 1 && i != hole + 2) { 
+         this.addPillar(800, i * 32 + 280, i);   
+       }
+  },
+  addPillar: function(x,y, i) {
+    var p = this.add.sprite(x, y, this.playerbmd, 0)
+    p.checkWorldBounds = true;
+    p.outOfBoundsKill = true;
 
+    if (i % 2) {
+      p.tint = 0xffffff;
+    }else {
+      p.tint = 0xff0000;
+    }
+    this.game.physics.arcade.enable(p);
+
+    p.body.velocity.x = -355; 
+  },
   update: function() {
 
     this.game.physics.arcade.collide(group, ground);
+    this.playerMovement();
+
     scrollPosition -= 6;
-    
     ground.tilePosition.x = scrollPosition;
     background.tilePosition.x = scrollPosition * 0.1;
 
+  },
+  playerMovement: function() {
     if (spaceKey.isDown && player.body.touching.down) {
       player.body.velocity.y = -600;
         this.game.add.tween(player).to({angle: player.angle - 270}, 800, Phaser.Easing.Linear.None).start();
@@ -102,25 +135,7 @@ Game.Play.prototype = {
         player.body.velocity.y = -200;
       }
     },this);
-
-
-    // background.tilePosition.x = -(scrollPosition * 0.005);
-    // // Toggle Music
-    // muteKey.onDown.add(this.toggleMute, this);
-
   },
-  // toggleMute: function() {
-  //   if (musicOn == true) {
-  //     musicOn = false;
-  //     this.music.volume = 0;
-  //   }else {
-  //     musicOn = true;
-  //     this.music.volume = 0.5;
-  //   }
-  // },
-  // render: function() {
-  //   game.debug.text('Health: ' + tri.health, 32, 96);
-  // }
   render: function()
   {
       this.game.debug.text(this.game.time.fps || '--', 2, 14, '#00ff00');
